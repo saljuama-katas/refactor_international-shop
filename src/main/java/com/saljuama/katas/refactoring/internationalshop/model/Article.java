6a550a963a58575bb33105826f1cbbfb09599afe
Salvador Juan Martinez <saljuama@gmail.com>
2020-03-22 02:52:41 +0100

    add withers

--- src/main/java/com/saljuama/katas/refactoring/internationalshop/model/Article.java
+++ src/main/java/com/saljuama/katas/refactoring/internationalshop/model/Article.java
@@ -1,16 +1,16 @@
 package com.saljuama.katas.refactoring.internationalshop.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "articles")
 @Data
+@With
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
+@AllArgsConstructor
 public class Article extends AuditableEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
