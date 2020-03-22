82d2c6e8c8a07235465b61b5a656b2f83949658e
Salvador Juan Martinez <saljuama@gmail.com>
2020-03-22 02:52:14 +0100

    enable finding contracts by region

--- src/main/java/com/saljuama/katas/refactoring/internationalshop/model/ContractRepository.java
+++ src/main/java/com/saljuama/katas/refactoring/internationalshop/model/ContractRepository.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ContractRepository extends JpaRepository<Contract, Long> {
   List<Contract> findByCustomerId(String customerId);
+
+  List<Contract> findByRegion(Region region);
 }
--- src/test/java/com/saljuama/katas/refactoring/internationalshop/model/ContractRepositoryTest.java
+++ src/test/java/com/saljuama/katas/refactoring/internationalshop/model/ContractRepositoryTest.java
@@ -25,13 +25,13 @@ class ContractRepositoryTest extends PersistenceIntegrationTest {
   @Test
   void canSaveANewContract() {
     assertEquals(0L, repository.count());
-    repository.save(new Contract("customer1", savedRegion(), Category.MUSIC, 5, LocalDate.now().minusDays(10), null));
+    repository.save(new Contract("customer1", savedRegion("Barcelona", "08001,08002"), Category.MUSIC, 5, LocalDate.now().minusDays(10), null));
     assertEquals(1L, repository.count());
   }
 
   @Test
   void findContractsByCustomer() {
-    Region savedRegion = savedRegion();
+    Region savedRegion = savedRegion("Barcelona", "08001,08002");
     repository.save(new Contract("customer1", savedRegion, Category.MUSIC, 5, LocalDate.now().minusDays(10), null));
     repository.save(new Contract("customer2", savedRegion, Category.MUSIC, 6, LocalDate.now().minusDays(12), null));
     repository.save(new Contract("customer1", savedRegion, Category.MUSIC, 7, LocalDate.now().minusDays(15), null));
@@ -41,7 +41,21 @@ class ContractRepositoryTest extends PersistenceIntegrationTest {
     assertEquals(2, result.size());
   }
 
-  private Region savedRegion() {
-    return regionRepository.save(new Region("Barcelona", "08001,08002"));
+  @Test
+  void findByRegion() {
+    Region barcelona = savedRegion("Barcelona", "08001,08002");
+    Region valencia = savedRegion("Valencia", "46001,46002");
+    repository.save(new Contract("customer1", barcelona, Category.MUSIC, 5, LocalDate.now().minusDays(10), null));
+    repository.save(new Contract("customer1", valencia, Category.MUSIC, 7, LocalDate.now().minusDays(15), null));
+    repository.save(new Contract("customer2", barcelona, Category.MUSIC, 6, LocalDate.now().minusDays(12), null));
+    repository.save(new Contract("customer3", barcelona, Category.MUSIC, 6, LocalDate.now().minusDays(12), null));
+    repository.save(new Contract("customer4", valencia, Category.MUSIC, 7, LocalDate.now().minusDays(15), null));
+
+    assertEquals(3, repository.findByRegion(barcelona).size());
+    assertEquals(2, repository.findByRegion(valencia).size());
+  }
+
+  private Region savedRegion(String city, String postalCodes) {
+    return regionRepository.save(new Region(city, postalCodes));
   }
 }
